c5daccc56dd959522e2c82f2d676bb29daf6cac2
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-27 21:18:02 -0400

    Initial Commit

---
+++ Magic 8 Ball ProgrammaticallyTests/Magic_8_Ball_ProgrammaticallyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Magic_8_Ball_ProgrammaticallyTests.swift
+//  Magic 8 Ball ProgrammaticallyTests
+//
+//  Created by Haamed Sultani on 9/27/18.
+//  Copyright © 2018 Sultani. All rights reserved.
+//
+
+import XCTest
+@testable import Magic_8_Ball_Programmatically
+
+class Magic_8_Ball_ProgrammaticallyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
